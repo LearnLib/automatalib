a07c30b47b1d609b0b28edd6b2c5015f689dd868
mvolpato <volpato.michele@gmail.com>
2014-05-02 16:05:32 +0200

    Added interface for input/output labels

---
+++ api/src/main/java/net/automatalib/words/InputOutputLabel.java
@@ -0,0 +1,44 @@
+/* Copyright (C) 2014 AutomataLib
+ * This file is part of AutomataLib, http://www.automatalib.net/.
+ * 
+ * AutomataLib is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 3.0 as published by the Free Software Foundation.
+ * 
+ * AutomataLib is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with AutomataLib; if not, see
+ * http://www.gnu.de/documents/lgpl.en.html.
+ */
+package net.automatalib.words;
+
+/**
+ * Interface for labels distinguishable as inputs or outputs.
+ * 
+ * @author Michele Volpato
+ *
+ * @param <I> input class
+ * @param <O> output class
+ */
+public interface InputOutputLabel<I,O> {
+	
+    /**
+     * Returns true if the label is an input.
+     */
+    public abstract boolean isInput();
+    
+    /**
+     * Returns true if the label is an output.
+     */
+    public abstract boolean isOutput();
+    
+    /**
+     * Return the label.
+     */
+    public abstract Object getLabel();
+
+}
