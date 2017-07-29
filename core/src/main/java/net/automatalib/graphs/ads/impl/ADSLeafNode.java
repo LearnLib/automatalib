4b75b75766e29923f43c4ab858b936b79ca694d8
Markus Frohme <markus.frohme@udo.edu>
2017-07-29 14:17:12 +0200

    api+core: refactor ADSs to be more inheritance friendly

--- core/src/main/java/net/automatalib/graphs/ads/impl/ADSLeafNode.java
+++ core/src/main/java/net/automatalib/graphs/ads/impl/ADSLeafNode.java
@@ -17,68 +17,18 @@ package net.automatalib.graphs.ads.impl;
 
 import net.automatalib.graphs.ads.ADSNode;
 
-import java.util.Collections;
-import java.util.Map;
-
 /**
- * Implementation of a leaf node
+ * An ADS-specific implementation of a leaf node
  *
  * @param <S> (hypothesis) state type
  * @param <I> input alphabet type
  * @param <O> output alphabet type
  * @author frohme
  */
-public class ADSLeafNode<S, I, O> implements ADSNode<S, I, O> {
-
-	private ADSNode<S, I, O> parent;
-	private S hypothesisState;
+public class ADSLeafNode<S, I, O> extends AbstractRecursiveADSLeafNode<S, I, O, ADSNode<S, I, O>>
+		implements ADSNode<S, I, O> {
 
 	public ADSLeafNode(ADSNode<S, I, O> parent, S hypothesisState) {
-		this.parent = parent;
-		this.hypothesisState = hypothesisState;
-	}
-
-	@Override
-	public boolean isLeaf() {
-		return true;
-	}
-
-	@Override
-	public S getHypothesisState() {
-		return this.hypothesisState;
-	}
-
-	@Override
-	public void setHypothesisState(S state) {
-		this.hypothesisState = state;
-	}
-
-	@Override
-	public I getSymbol() {
-		return null;
-	}
-
-	@Override
-	public void setSymbol(I symbol) {
-		throw new UnsupportedOperationException("Cannot set symbol state on a leaf node");}
-
-	@Override
-	public ADSNode<S, I, O> getParent() {
-		return this.parent;
-	}
-
-	@Override
-	public void setParent(ADSNode<S, I, O> parent) {
-		this.parent = parent;
-	}
-
-	@Override
-	public Map<O, ADSNode<S, I, O>> getChildren() {
-		return Collections.emptyMap();
-	}
-
-	@Override
-	public String toString() {
-		return this.getHypothesisState() == null ? "<null>" : this.getHypothesisState().toString();
+		super(parent, hypothesisState);
 	}
 }
