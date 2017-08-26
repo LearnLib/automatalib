4eb010b9e3b81447b303e0831c9265d51aadee67
Markus Frohme <markus.frohme@udo.edu>
2017-08-26 21:34:40 +0200

    make Covers test actually test something

--- util/src/test/java/net/automatalib/util/automata/CoversTest.java
+++ util/src/test/java/net/automatalib/util/automata/CoversTest.java
@@ -23,65 +23,75 @@ import net.automatalib.automata.fsa.impl.compact.CompactDFA;
 import net.automatalib.words.Alphabet;
 import net.automatalib.words.Word;
 import net.automatalib.words.impl.Alphabets;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 /**
- * FIXME: Turn this into a real test.
- *
  * @author Malte Isberner
+ * @author frohme
  */
 @Test
 public class CoversTest {
 
+    private final Alphabet<Integer> alphabet = Alphabets.integers(0, 2);
+    final CompactDFA<Integer> dfa = new CompactDFA<>(alphabet);
+
+    private final List<Word<Integer>> stateCover = new ArrayList<>();
+    private final List<Word<Integer>> transCover = new ArrayList<>();
+    private final List<Word<Integer>> structuralCover = new ArrayList<>();
+
+    private final List<Word<Integer>> newStates = new ArrayList<>();
+    private final List<Word<Integer>> newTransitions = new ArrayList<>();
+    private final List<Word<Integer>> newStructural = new ArrayList<>();
+
+    private final List<Word<Integer>> expectedNewStateCover = new ArrayList<>();
+    private final List<Word<Integer>> expectedNewTransCover = new ArrayList<>();
+    private final List<Word<Integer>> expectedNewStructuralCover = new ArrayList<>();
+
+    private static final List<Word<Integer>> EMPTY = Collections.emptyList();
+
     @Test
     public void testIncrementalCover() {
-        Alphabet<Integer> alphabet = Alphabets.integers(0, 2);
-        CompactDFA<Integer> dfa = new CompactDFA<>(alphabet);
-
-        List<Word<Integer>> stateCover = new ArrayList<>(), transCover = new ArrayList<>();
-        List<Word<Integer>> structuralCover = new ArrayList<>();
-
-        List<Word<Integer>> newStates = new ArrayList<>(), newTransitions = new ArrayList<>();
-        List<Word<Integer>> newStructural = new ArrayList<>();
-
-        List<Word<Integer>> empty = Collections.emptyList();
-
+        // initial configuration
         int q0 = dfa.addInitialState();
         dfa.addTransition(q0, 0, q0);
         dfa.addTransition(q0, 1, q0);
         dfa.addTransition(q0, 2, q0);
 
-        Covers.incrementalCover(dfa, alphabet, stateCover, transCover, newStates, newTransitions);
-        System.err.println("New states: " + newStates + ", new transitions: " + newTransitions);
-        stateCover.addAll(newStates);
-        transCover.addAll(newTransitions);
-        newStates.clear();
-        newTransitions.clear();
+        expectedNewStateCover.add(Word.epsilon());
+        expectedNewTransCover.add(Word.fromLetter(0));
+        expectedNewTransCover.add(Word.fromLetter(1));
+        expectedNewTransCover.add(Word.fromLetter(2));
+        expectedNewStructuralCover.addAll(expectedNewStateCover);
+        expectedNewStructuralCover.addAll(expectedNewTransCover);
 
-        Covers.incrementalCover(dfa, alphabet, structuralCover, empty, newStructural, newStructural);
-        System.err.println("New structural: " + newStructural);
-        structuralCover.addAll(newStructural);
-        newStructural.clear();
+        Covers.incrementalCover(dfa, alphabet, EMPTY, EMPTY, newStates, newTransitions);
+        Covers.incrementalCover(dfa, alphabet, EMPTY, EMPTY, newStructural, newStructural);
 
-        int q1 = dfa.addState(true);
+        checkCovers();
+        updateAndClearCovers();
+
+        // second configuration
+        int q1 = dfa.addState();
         dfa.addTransition(q1, 0, q1);
         dfa.addTransition(q1, 1, q0);
         dfa.addTransition(q1, 2, q1);
 
         dfa.setTransition(q0, 0, q1);
 
+        expectedNewStateCover.add(Word.fromLetter(0));
+        expectedNewTransCover.add(Word.fromSymbols(0, 0));
+        expectedNewTransCover.add(Word.fromSymbols(0, 1));
+        expectedNewTransCover.add(Word.fromSymbols(0, 2));
+        expectedNewStructuralCover.addAll(expectedNewTransCover);
+
         Covers.incrementalCover(dfa, alphabet, stateCover, transCover, newStates, newTransitions);
-        System.err.println("New states: " + newStates + ", new transitions: " + newTransitions);
-        stateCover.addAll(newStates);
-        transCover.addAll(newTransitions);
-        newStates.clear();
-        newTransitions.clear();
+        Covers.incrementalCover(dfa, alphabet, structuralCover, EMPTY, newStructural, newStructural);
 
-        Covers.incrementalCover(dfa, alphabet, structuralCover, empty, newStructural, newStructural);
-        System.err.println("New structural: " + newStructural);
-        structuralCover.addAll(newStructural);
-        newStructural.clear();
+        checkCovers();
+        updateAndClearCovers();
 
+        // third configuration
         int q2 = dfa.addState();
         dfa.addTransition(q2, 0, q2);
         dfa.addTransition(q2, 1, q2);
@@ -90,17 +100,36 @@ public class CoversTest {
         dfa.setTransition(q1, 2, q2);
         dfa.setTransition(q0, 2, q2);
 
+        expectedNewStateCover.add(Word.fromLetter(2));
+        expectedNewTransCover.add(Word.fromSymbols(2, 0));
+        expectedNewTransCover.add(Word.fromSymbols(2, 1));
+        expectedNewTransCover.add(Word.fromSymbols(2, 2));
+        expectedNewStructuralCover.addAll(expectedNewTransCover);
+
         Covers.incrementalCover(dfa, alphabet, stateCover, transCover, newStates, newTransitions);
-        System.err.println("New states: " + newStates + ", new transitions: " + newTransitions);
+        Covers.incrementalCover(dfa, alphabet, structuralCover, EMPTY, newStructural, newStructural);
+
+        checkCovers();
+    }
+
+    private void checkCovers() {
+        Assert.assertEquals(newStates, expectedNewStateCover);
+        Assert.assertEquals(newTransitions, expectedNewTransCover);
+        Assert.assertEquals(newStructural, expectedNewStructuralCover);
+    }
+
+    private void updateAndClearCovers() {
         stateCover.addAll(newStates);
         transCover.addAll(newTransitions);
+        structuralCover.addAll(newStructural);
+
         newStates.clear();
         newTransitions.clear();
-
-        Covers.incrementalCover(dfa, alphabet, structuralCover, empty, newStructural, newStructural);
-        System.err.println("New structural: " + newStructural);
-        structuralCover.addAll(newStructural);
         newStructural.clear();
+
+        expectedNewStateCover.clear();
+        expectedNewTransCover.clear();
+        expectedNewStructuralCover.clear();
     }
 
 }
