7576886c553198204473d4aa64dda81b4a80c74b
Markus Frohme <markus.frohme@udo.edu>
2018-10-06 00:52:20 +0200

    run examples as part of integration tests

---
+++ examples/src/test/java/net/automatalib/examples/ExamplesIT.java
@@ -0,0 +1,107 @@
+/* Copyright (C) 2013-2018 TU Dortmund
+ * This file is part of AutomataLib, http://www.automatalib.net/.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.automatalib.examples;
+
+import java.awt.AWTEvent;
+import java.awt.Toolkit;
+import java.awt.Window;
+import java.awt.event.WindowEvent;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+
+import javax.swing.SwingUtilities;
+
+import net.automatalib.commons.util.system.JVMUtil;
+import net.automatalib.examples.brics.SimpleBricsExample;
+import net.automatalib.examples.dot.DOTExample;
+import net.automatalib.examples.incremental.IncrementalDFAExample;
+import net.automatalib.examples.incremental.IncrementalMealyExample;
+import net.automatalib.examples.incremental.IncrementalPCDFAExample;
+import net.automatalib.examples.modelchecking.LTSminExample;
+import net.automatalib.modelcheckers.ltsmin.LTSminUtil;
+import org.testng.SkipException;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+/**
+ * Run the examples as part of integration testing.
+ *
+ * @author frohme
+ */
+public class ExamplesIT {
+
+    @BeforeClass
+    public void setupAutoClose() {
+        // As soon as we observe an event that indicates a new window, close it to prevent blocking the ITs.
+        Toolkit.getDefaultToolkit().addAWTEventListener(event -> {
+            final WindowEvent windowEvent = (WindowEvent) event;
+            final Window w = windowEvent.getWindow();
+            w.dispatchEvent(new WindowEvent(w, WindowEvent.WINDOW_CLOSING));
+        }, AWTEvent.WINDOW_FOCUS_EVENT_MASK);
+    }
+
+    @Test
+    public void testSimpleBricsExample() throws InvocationTargetException, InterruptedException {
+        checkJVMCompatibility();
+        SwingUtilities.invokeAndWait(() -> SimpleBricsExample.main(new String[0]));
+    }
+
+    @Test
+    public void testDOTExample() throws InvocationTargetException, InterruptedException {
+        checkJVMCompatibility();
+        SwingUtilities.invokeAndWait(() -> {
+            try {
+                DOTExample.main(new String[0]);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+
+    @Test
+    public void testIncrementalDFAExample() throws InvocationTargetException, InterruptedException {
+        checkJVMCompatibility();
+        SwingUtilities.invokeAndWait(() -> IncrementalDFAExample.main(new String[0]));
+    }
+
+    @Test
+    public void testIncrementalMealyExample() throws InvocationTargetException, InterruptedException {
+        checkJVMCompatibility();
+        SwingUtilities.invokeAndWait(() -> IncrementalMealyExample.main(new String[0]));
+    }
+
+    @Test
+    public void testIncrementalPCDFAExample() throws InvocationTargetException, InterruptedException {
+        checkJVMCompatibility();
+        SwingUtilities.invokeAndWait(() -> IncrementalPCDFAExample.main(new String[0]));
+    }
+
+    @Test
+    public void testLTSminExample() {
+        if (!LTSminUtil.checkUsable()) {
+            throw new SkipException("LTSmin is not installed");
+        }
+
+        LTSminExample.main(new String[0]);
+    }
+
+    private static void checkJVMCompatibility() {
+        if (JVMUtil.getCanonicalSpecVersion() > 8) {
+            throw new SkipException("The headless AWT environment currently only works with Java 8 and below");
+        }
+    }
+
+}
