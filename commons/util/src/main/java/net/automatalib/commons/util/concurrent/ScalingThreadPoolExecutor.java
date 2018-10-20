a26c5e2a8ca43aaeace74fe682eace45abac7547
Markus Frohme <markus.frohme@udo.edu>
2018-10-20 16:58:40 +0200

    util: add ScalingThreadPoolExecutor
    currently mainly used by the DynamicParallelOracle of LearnLib

---
+++ commons/util/src/main/java/net/automatalib/commons/util/concurrent/ScalingThreadPoolExecutor.java
@@ -0,0 +1,115 @@
+/* Copyright (C) 2013-2018 TU Dortmund
+ * This file is part of AutomataLib, http://www.automatalib.net/.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.automatalib.commons.util.concurrent;
+
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.RejectedExecutionHandler;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import javax.annotation.Nonnull;
+
+/**
+ * A {@link ThreadPoolExecutor} that internally uses a {@link ScalingLinkedBlockingQueue} to manage scheduled tasks.
+ * This allows us to manage a dynamically sized thread pool that actually spawns new threads when the pool still allows
+ * for it.
+ * <p>
+ * Additionally, this implementation keeps track of the current number of active threads by using an
+ * {@link AtomicInteger} counter, rather than querying its list of worker threads.
+ * <p>
+ * For further information, see
+ * <a href="https://github.com/kimchy/kimchy.github.com/blob/master/_posts/2008-11-23-juc-executorservice-gotcha.textile">
+ * this blog post</a>.
+ *
+ * @author frohme
+ */
+public class ScalingThreadPoolExecutor extends ThreadPoolExecutor {
+
+    private final AtomicInteger activeCount = new AtomicInteger();
+
+    public ScalingThreadPoolExecutor(int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit) {
+        super(corePoolSize, maximumPoolSize, keepAliveTime, unit, new ScalingLinkedBlockingQueue());
+
+        ((ScalingLinkedBlockingQueue) getQueue()).setTpe(this);
+        setRejectedExecutionHandler(new ForceEnqueuingHandler());
+    }
+
+    @Override
+    public int getActiveCount() {
+        return activeCount.get();
+    }
+
+    @Override
+    protected void beforeExecute(Thread t, Runnable r) {
+        activeCount.incrementAndGet();
+    }
+
+    @Override
+    protected void afterExecute(Runnable r, Throwable t) {
+        activeCount.decrementAndGet();
+    }
+
+    /**
+     * A modified {@link LinkedBlockingQueue} that rejects offering new elements if its associated
+     * {@link ScalingThreadPoolExecutor} still has threads to spare. This means we will force a creation of a new thread
+     * whenever there is room for parallel processing.
+     * <p>
+     * To prevent {@link RejectedExecutionException}s when reaching the limit of the thread pool, this class is only
+     * useful in combination with a {@link ForceEnqueuingHandler} that forcefully enqueues the scheduled task to this
+     * (otherwise unbounded) queue.
+     *
+     * @author frohme
+     */
+    static class ScalingLinkedBlockingQueue extends LinkedBlockingQueue<Runnable> {
+
+        private static final long serialVersionUID = 7063154517431823515L;
+        private transient ScalingThreadPoolExecutor tpe;
+
+        void setTpe(ScalingThreadPoolExecutor tpe) {
+            this.tpe = tpe;
+        }
+
+        @Override
+        public boolean offer(@Nonnull Runnable r) {
+            if (tpe != null && tpe.getActiveCount() + size() < tpe.getMaximumPoolSize()) {
+                return false;
+            }
+
+            return super.offer(r);
+        }
+    }
+
+    /**
+     * A {@link RejectedExecutionHandler} that forces the enqueuing of rejected tasks to the queue of a
+     * {@link ThreadPoolExecutor}. Mainly useful in combination with {@link ScalingLinkedBlockingQueue}.
+     *
+     * @author frohme
+     */
+    static class ForceEnqueuingHandler implements RejectedExecutionHandler {
+
+        @Override
+        public void rejectedExecution(Runnable r, ThreadPoolExecutor executor) {
+            try {
+                executor.getQueue().put(r);
+            } catch (InterruptedException e) {
+                throw new RejectedExecutionException(e);
+            }
+        }
+    }
+
+}
---
+++ commons/util/src/test/java/net/automatalib/commons/util/concurrent/ScalingThreadPoolExecutorTest.java
@@ -0,0 +1,102 @@
+/* Copyright (C) 2013-2018 TU Dortmund
+ * This file is part of AutomataLib, http://www.automatalib.net/.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.automatalib.commons.util.concurrent;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
+import org.testng.annotations.Test;
+import org.testng.internal.thread.ThreadTimeoutException;
+
+/**
+ * @author frohme
+ */
+public class ScalingThreadPoolExecutorTest {
+
+    @Test(timeOut = 2000)
+    public void testCachedThreadCreation() throws ExecutionException, InterruptedException {
+
+        final int tasks = 10;
+        final CountDownLatch latch = new CountDownLatch(tasks);
+
+        final ScalingThreadPoolExecutor executor = new ScalingThreadPoolExecutor(0, tasks, 60L, TimeUnit.SECONDS);
+        final List<Future<?>> futures = new ArrayList<>(tasks);
+
+        for (int i = 0; i < tasks; i++) {
+            futures.add(executor.submit(() -> {
+                try {
+                    latch.countDown();
+                    latch.await();
+                } catch (InterruptedException e) {
+                    throw new IllegalStateException(e);
+                }
+            }));
+        }
+
+        // this code only returns, if 'tasks' threads are spawned, which all decrease the shared latch
+        for (final Future<?> f : futures) {
+            f.get();
+        }
+
+        executor.shutdown();
+    }
+
+    @Test(timeOut = 2000)
+    public void testThreadSchedulingNormal() throws ExecutionException, InterruptedException {
+        testThreadScheduling(2);
+    }
+
+    @Test(timeOut = 1000, expectedExceptions = ThreadTimeoutException.class)
+    public void testCachedThreadSchedulingTimeout() throws ExecutionException, InterruptedException {
+        testThreadScheduling(1);
+    }
+
+    private static void testThreadScheduling(int availableThreads) throws ExecutionException, InterruptedException {
+
+        final int tasks = 10;
+        final CountDownLatch latch = new CountDownLatch(tasks - 1);
+
+        final ScalingThreadPoolExecutor executor =
+                new ScalingThreadPoolExecutor(0, availableThreads, 60L, TimeUnit.SECONDS);
+        final List<Future<?>> futures = new ArrayList<>(tasks);
+
+        // submit a long-running task
+        futures.add(executor.submit(() -> {
+            try {
+                latch.await();
+            } catch (InterruptedException e) {
+                throw new IllegalStateException(e);
+            }
+        }));
+
+        // submit remaining short-running tasks
+        for (int i = 0; i < tasks - 1; i++) {
+            futures.add(executor.submit(latch::countDown));
+        }
+
+        // this code only returns, if the short-running tasks were scheduled 9 times to unblock the long-running task
+        for (final Future<?> f : futures) {
+            f.get();
+        }
+
+        executor.shutdown();
+    }
+
+}
