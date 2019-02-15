227555ed01e022f43602bd6424e18d31c62bf047
Markus Frohme <markus.frohme@udo.edu>
2019-02-15 13:00:44 +0100

    update archetypes
    - remove license header from generated pom.xml because the generated project is not part of AutomataLib
    - do not generate an empty src/main/test folder
    - add a small Example.java and logback.xml config to generated projects
    - remove redundant archetype-metadata.xml files

---
+++ archetypes/basic/src/main/resources/archetype-resources/src/main/java/Example.java
@@ -0,0 +1,33 @@
+package ${package};
+
+import net.automatalib.automata.transducers.MealyMachine;
+import net.automatalib.util.automata.builders.AutomatonBuilders;
+import net.automatalib.visualization.Visualization;
+import net.automatalib.words.Alphabet;
+import net.automatalib.words.impl.Alphabets;
+
+/**
+ * An example class that creates a simple Mealy Machine and displays it via the JUNG library.
+ *
+ * @author frohme
+ */
+public final class Example {
+
+    private Example() {
+        // prevent instantiation
+    }
+
+    public static void main(String[] args) {
+
+        final Alphabet<Character> inputAlphabet = Alphabets.characters('a', 'c');
+
+        MealyMachine<?, Character, ?, Character> mealy = AutomatonBuilders.<Character, Character>newMealy(inputAlphabet)
+                .withInitial("q0")
+                .from("q0").on('a').withOutput('1').to("q1")
+                .from("q1").on('b').withOutput('2').to("q2")
+                .from("q2").on('c').withOutput('3').to("q0")
+                .create();
+
+        Visualization.visualize(mealy.transitionGraphView(inputAlphabet));
+    }
+}
