f0f95c6787c51fc0160e61b0dec8595ee40e96c3
Markus Frohme <markus.frohme@udo.edu>
2019-06-05 14:06:06 +0200

    add product automata construction for Mealy Machines

---
+++ util/src/main/java/net/automatalib/util/automata/transducers/MealyMachines.java
@@ -0,0 +1,92 @@
+/* Copyright (C) 2013-2019 TU Dortmund
+ * This file is part of AutomataLib, http://www.automatalib.net/.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.automatalib.util.automata.transducers;
+
+import java.util.Collection;
+
+import net.automatalib.automata.transducers.MealyMachine;
+import net.automatalib.automata.transducers.MutableMealyMachine;
+import net.automatalib.automata.transducers.impl.compact.CompactMealy;
+import net.automatalib.commons.util.Pair;
+import net.automatalib.ts.output.MealyTransitionSystem;
+import net.automatalib.util.ts.copy.TSCopy;
+import net.automatalib.util.ts.transducers.Transducers;
+import net.automatalib.util.ts.traversal.TSTraversal;
+import net.automatalib.util.ts.traversal.TSTraversalMethod;
+import net.automatalib.words.Alphabet;
+
+public final class MealyMachines {
+
+    private MealyMachines() {}
+
+    /**
+     * Constructs the product automaton for two Mealy Machines.
+     *
+     * @param mealy1
+     *         the first Mealy Machine
+     * @param mealy2
+     *         the second Mealy Machine
+     * @param inputAlphabet
+     *         the input alphabet of the returned automaton
+     * @param <I>
+     *         input symbol type
+     * @param <O1>
+     *         output symbol type of the first Mealy Machine
+     * @param <O2>
+     *         output symbol type of the second Mealy Machine
+     *
+     * @return a new Mealy Machine representing the product automaton of the specified Mealy Machines
+     */
+    public static <I, O1, O2> CompactMealy<I, Pair<O1, O2>> combine(MealyMachine<?, I, ?, O1> mealy1,
+                                                                    MealyMachine<?, I, ?, O2> mealy2,
+                                                                    Alphabet<I> inputAlphabet) {
+        return combine(mealy1, mealy2, inputAlphabet, new CompactMealy<>(inputAlphabet));
+    }
+
+    /**
+     * Constructs the product automaton for two Mealy Machines.
+     *
+     * @param mealy1
+     *         the first Mealy Machine
+     * @param mealy2
+     *         the second Mealy Machine
+     * @param inputs
+     *         the input symbols to consider
+     * @param out
+     *         the instance to which the combined automaton should be written
+     * @param <I>
+     *         input symbol type
+     * @param <O1>
+     *         output symbol type of the first Mealy Machine
+     * @param <O2>
+     *         output symbol type of the second Mealy Machine
+     *
+     * @return {@code out}, for convenience
+     */
+    public static <I, O1, O2, A extends MutableMealyMachine<?, I, ?, Pair<O1, O2>>> A combine(MealyMachine<?, I, ?, O1> mealy1,
+                                                                                              MealyMachine<?, I, ?, O2> mealy2,
+                                                                                              Collection<? extends I> inputs,
+                                                                                              A out) {
+        final MealyTransitionSystem<?, I, ?, Pair<O1, O2>> comb = Transducers.combine(mealy1, mealy2);
+
+        TSCopy.copy(TSTraversalMethod.BREADTH_FIRST,
+                    comb,
+                    TSTraversal.NO_LIMIT,
+                    inputs,
+                    (MutableMealyMachine<?, I, ?, Pair<O1, O2>>) out);
+        return out;
+    }
+}
