bbe1dcadd8bce71fa6032923195cf7893b981df6
Conturing <13372078+Conturing@users.noreply.github.com>
2020-07-07 11:05:03 +0200

    Improve error message when conjunction fails.
    - Use DFS to find the path from root to node/transition where the error occurs
    - Generate error message based on that path

--- util/src/main/java/net/automatalib/util/ts/modal/ModalConjunction.java
+++ util/src/main/java/net/automatalib/util/ts/modal/ModalConjunction.java
@@ -15,18 +15,26 @@
  */
 package net.automatalib.util.ts.modal;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Deque;
+import java.util.List;
 import java.util.Map;
 
 import net.automatalib.automata.AutomatonCreator;
+import net.automatalib.automata.graphs.TransitionEdge;
 import net.automatalib.commons.util.Pair;
+import net.automatalib.graphs.Graph;
 import net.automatalib.ts.modal.ModalEdgeProperty;
 import net.automatalib.ts.modal.ModalTransitionSystem;
 import net.automatalib.ts.modal.MutableModalEdgeProperty;
 import net.automatalib.ts.modal.MutableModalTransitionSystem;
 import net.automatalib.util.fixedpoint.WorksetMappingAlgorithm;
+import net.automatalib.util.graphs.traversal.DFSVisitor;
+import net.automatalib.util.graphs.traversal.GraphTraversal;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -156,6 +164,7 @@ class ModalConjunction<A extends MutableModalTransitionSystem<S, I, T, TP>, S, S
                 LOGGER.debug("current transition 1: {}", transition1);
 
                 if (mc1.getTransitionProperty(transition1).isMust() && transitions0.isEmpty()) {
+                    LOGGER.warn(traceError(transition1));
                     throw new IllegalArgumentException("error in conjunction");
                 }
             }
@@ -168,4 +177,91 @@ class ModalConjunction<A extends MutableModalTransitionSystem<S, I, T, TP>, S, S
         return result;
     }
 
+    protected String traceError(T1 transition1) {
+        EdgeTracer<S1, I, T1> finder = new EdgeTracer<>(transition1);
+
+        GraphTraversal.dfs(mc1.transitionGraphView(mc1.getInputAlphabet()),
+                           mc1.getInitialStates(),
+                           finder);
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("Error in conjunction. Transition \"");
+        sb.append(transition1);
+        sb.append("\" has no partner. Sequence leading to transition: ");
+        for (S1 state : finder.getStateSequence()) {
+            sb.append(state);
+            sb.append(",");
+        }
+        sb.deleteCharAt(sb.length() - 1);
+        return sb.toString();
+    }
+
+    private static class EdgeTracer<S, I, T> implements DFSVisitor<S, TransitionEdge<I, T>, Void> {
+
+        Deque<S> stateStack;
+        boolean freeze;
+        T targetTransition;
+
+        public EdgeTracer(T transition) {
+            stateStack = new ArrayDeque<>();
+            freeze = false;
+            targetTransition = transition;
+        }
+
+        @Override
+        public Void initialize(S node) {
+            return null;
+        }
+
+        @Override
+        public void explore(S node, Void data) {
+            if (!freeze) {
+                stateStack.push(node);
+            }
+        }
+
+        @Override
+        public void finish(S node, Void data) {
+            if (!freeze) {
+                S top = stateStack.pop();
+                assert top == node;
+            }
+        }
+
+        public void testEdge(TransitionEdge<I, T> edge) {
+            if (edge.getTransition() == targetTransition) {
+                freeze = true;
+            }
+        }
+
+        public List<S> getStateSequence() {
+            return new ArrayList<>(stateStack);
+        }
+
+        @Override
+        public Void treeEdge(S srcNode, Void srcData, TransitionEdge<I, T> edge, S tgtNode) {
+            testEdge(edge);
+            return null;
+        }
+
+        @Override
+        public void backEdge(S srcNode, Void srcData, TransitionEdge<I, T> edge, S tgtNode, Void tgtData) {
+            testEdge(edge);
+        }
+
+        @Override
+        public void crossEdge(S srcNode, Void srcData, TransitionEdge<I, T> edge, S tgtNode, Void tgtData) {
+            testEdge(edge);
+        }
+
+        @Override
+        public void forwardEdge(S srcNode, Void srcData, TransitionEdge<I, T> edge, S tgtNode, Void tgtData) {
+            testEdge(edge);
+        }
+
+        @Override
+        public void backtrackEdge(S srcNode, Void srcDate, TransitionEdge<I, T> edge, S tgtNode, Void tgtData) {
+            testEdge(edge);
+        }
+    }
 }
