032ead5098417db5faf7cb5093a356e5a163c1b3
Markus Frohme <markus.frohme@udo.edu>
2025-01-06 19:37:44 +0100

    formatting

--- core/src/main/java/net/automatalib/modelchecking/impl/AbstractUnfoldingModelChecker.java
+++ core/src/main/java/net/automatalib/modelchecking/impl/AbstractUnfoldingModelChecker.java
@@ -21,8 +21,9 @@ import net.automatalib.modelchecking.ModelCheckerLasso;
 /**
  * An {@link ModelCheckerLasso} that can unfold loops of lassos.
  * <p>
- * Unfolding a lasso is done according to two conditions: 1. the lasso has to be unfolded a minimum number of times
- * ({@link #getMinimumUnfolds()}. 2. the lasso has to be unfolded relative to the number of states in a hypothesis,
+ * Unfolding a lasso is done according to two conditions:
+ * 1. the lasso has to be unfolded a minimum number of times ({@link #getMinimumUnfolds()}.
+ * 2. the lasso has to be unfolded relative to the number of states in a hypothesis,
  * multiplied by some double ({@link #getMultiplier()}.
  * <p>
  * Note that one can unfold a lasso a fixed number of times if the multiplier is set to {@code 0.0}.
