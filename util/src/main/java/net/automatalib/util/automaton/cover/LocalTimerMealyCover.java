20771b6e54d7153a711f323b2e5acae5522a6a63
Paul Kogel <p.kogel@tu-berlin.de>
2025-10-10 08:52:37 +0200

    Updated calculation of MMLT location cover to accept a list of inputs to be considered.

--- util/src/main/java/net/automatalib/util/automaton/cover/LocalTimerMealyCover.java
+++ util/src/main/java/net/automatalib/util/automaton/cover/LocalTimerMealyCover.java
@@ -1,35 +1,33 @@
 package net.automatalib.util.automaton.cover;
 
-import net.automatalib.alphabet.impl.GrowingMapAlphabet;
 import net.automatalib.alphabet.time.mmlt.LocalTimerMealySemanticInputSymbol;
-import net.automatalib.alphabet.time.mmlt.TimeoutSymbol;
 import net.automatalib.automaton.time.mmlt.LocalTimerMealy;
 import net.automatalib.automaton.time.mmlt.semantics.LocalTimerMealyConfiguration;
 import net.automatalib.word.Word;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class LocalTimerMealyCover {
 
-    public static <S, I, O> Map<S, Word<LocalTimerMealySemanticInputSymbol<I>>> getLocalTimerMealyLocationCover(LocalTimerMealy<S, I, O> automaton) {
-        return getLocalTimerMealyLocationCover(automaton, true);
+    public static <S, I, O> Map<S, Word<LocalTimerMealySemanticInputSymbol<I>>> getLocalTimerMealyLocationCover(LocalTimerMealy<S, I, O> automaton, Collection<LocalTimerMealySemanticInputSymbol<I>> inputs) {
+        return getLocalTimerMealyLocationCover(automaton, inputs, true);
     }
 
     /**
      * Calculates a location cover for an MMLT.
      * <p>
      * The cover provides one prefix for each location of the MMLT.
-     * The returned prefixes use inputs for the extended semantics.
+     * The returned prefixes use only the provided inputs.
+     * <p>
      * If some locations are isolated, they are excluded from the cover.
+     * If some locations cannot be reached with the provided inputs, they are also excluded.
      *
      * @param automaton       MMLT
+     * @param inputs          The inputs to use when calculating the cover. Must be an ordered collection.
      * @param allowIncomplete If set, no error is thrown if some locations are unreachable.
      * @return Location cover in format location -> prefix.
      */
-    public static <S, I, O> Map<S, Word<LocalTimerMealySemanticInputSymbol<I>>> getLocalTimerMealyLocationCover(LocalTimerMealy<S, I, O> automaton, boolean allowIncomplete) {
+    public static <S, I, O> Map<S, Word<LocalTimerMealySemanticInputSymbol<I>>> getLocalTimerMealyLocationCover(LocalTimerMealy<S, I, O> automaton, Collection<LocalTimerMealySemanticInputSymbol<I>> inputs, boolean allowIncomplete) {
         Map<S, Word<LocalTimerMealySemanticInputSymbol<I>>> locPrefixes = new HashMap<>();
         Map<LocalTimerMealyConfiguration<S, I, O>, Word<LocalTimerMealySemanticInputSymbol<I>>> cfgPrefixes = new HashMap<>();
         List<LocalTimerMealyConfiguration<S, I, O>> queue = new ArrayList<>();
@@ -38,12 +36,9 @@ public class LocalTimerMealyCover {
         cfgPrefixes.put(automaton.getSemantics().getInitialConfiguration(), Word.epsilon());
         locPrefixes.put(automaton.getInitialState(), Word.epsilon());
 
-        GrowingMapAlphabet<LocalTimerMealySemanticInputSymbol<I>> exploreAlphabet = new GrowingMapAlphabet<>(automaton.getUntimedAlphabet());
-        exploreAlphabet.add(new TimeoutSymbol<>());
-
         while (!queue.isEmpty()) {
             var current = queue.remove(0);
-            for (var symbol : exploreAlphabet) {
+            for (var symbol : inputs) {
                 var trans = automaton.getSemantics().getTransition(current, symbol);
                 if (trans.target().equals(current)) {
                     continue; // self-loop
