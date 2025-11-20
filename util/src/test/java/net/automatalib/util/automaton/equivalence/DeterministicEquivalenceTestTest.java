19ec93fc2e26ddd77c5db0a796b399706faff62e
Markus Frohme <markus.frohme@udo.edu>
2025-11-20 23:31:57 +0100

    cleanups

--- util/src/test/java/net/automatalib/util/automaton/equivalence/DeterministicEquivalenceTestTest.java
+++ util/src/test/java/net/automatalib/util/automaton/equivalence/DeterministicEquivalenceTestTest.java
@@ -136,7 +136,7 @@ public class DeterministicEquivalenceTestTest {
         testIndexComputation((int size1, int size2) -> new MapRegistry<>(size1));
     }
 
-    private static <I> void testIndexComputation(BiIntFunction<Registry<Integer>> constructor) {
+    private static void testIndexComputation(BiIntFunction<Registry<Integer>> constructor) {
         final int size1 = 3;
         final int size2 = 5;
         final Registry<Integer> registry = constructor.apply(size1, size2);
@@ -178,11 +178,11 @@ public class DeterministicEquivalenceTestTest {
                                                                                                                       M a2,
                                                                                                                       Alphabet<I> alphabet,
                                                                                                                       boolean equivalent) {
+        // explicitly assign type to (redundant) variables, otherwise javac complains
+        final UniversalDeterministicAutomaton<?, I, ?, ?, ?> m1 = a1;
+        final UniversalDeterministicAutomaton<?, I, ?, ?, ?> m2 = a2;
 
-        final Word<I> separatingWord =
-                DeterministicEquivalenceTest.findSeparatingWord((UniversalDeterministicAutomaton<?, I, ?, ?, ?>) a1,
-                                                                (UniversalDeterministicAutomaton<?, I, ?, ?, ?>) a2,
-                                                                alphabet);
+        final Word<I> separatingWord = DeterministicEquivalenceTest.findSeparatingWord(m1, m2, alphabet);
 
         Assert.assertEquals(equivalent, separatingWord == null);
 
