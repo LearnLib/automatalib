d99704042b8029de29577d89becf195000083447
Markus Frohme <markus.frohme@udo.edu>
2025-11-21 01:18:25 +0100

    jung: found workaround for crashing JVM

--- visualization/jung-visualizer/src/test/java/net/automatalib/visualization/jung/ProviderTest.java
+++ visualization/jung-visualizer/src/test/java/net/automatalib/visualization/jung/ProviderTest.java
@@ -31,9 +31,19 @@ import net.automatalib.visualization.VisualizationHelper;
 import net.automatalib.visualization.VisualizationProvider;
 import org.testng.Assert;
 import org.testng.SkipException;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class ProviderTest {
+
+    @BeforeClass
+    public void setupCacio() throws InterruptedException, InvocationTargetException {
+        if (shouldRunGUITests()) {
+            // hack: the static initializer of this class does the magic we want, so only invoke it on compatible JVMs
+            // hack2: run on the EDT so that we get invoked before JUNG triggers conflicting AWT initializations
+            SwingUtilities.invokeAndWait(CacioExtension::new);
+        }
+    }
 
     @Test
     public void testProviderConfiguration() {
@@ -50,7 +60,9 @@ public class ProviderTest {
     @Test(dependsOnMethods = "testProviderConfiguration", timeOut = 30000)
     public void testDisplay() throws InterruptedException, InvocationTargetException {
 
-        checkExecution();
+        if (!shouldRunGUITests()) {
+            throw new SkipException("The headless AWT environment is not supported on this platform");
+        }
 
         final Random random = new Random(42);
         final CompactDFA<Integer> dfa = RandomAutomata.randomDFA(random, 10, Alphabets.integers(1, 6));
@@ -61,14 +73,9 @@ public class ProviderTest {
                                                                    new RandomEdgeStyler<>(random)));
     }
 
-    private static void checkExecution() {
+    private static boolean shouldRunGUITests() {
         final int feature = Runtime.version().feature();
-        if (feature == 17 || feature == 21) {
-            // hack: the static initializer of this class does the magic we want, so only invoke it on compatible JVMs
-            new CacioExtension();
-        } else {
-            throw new SkipException("The headless AWT environment is not supported on this platform");
-        }
+        return feature == 17 || feature == 21;
     }
 
     private static final class RandomEdgeStyler<N, E> implements VisualizationHelper<N, E> {
