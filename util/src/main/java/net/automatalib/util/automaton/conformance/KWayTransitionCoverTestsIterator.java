6c2161f03c522a1dd20aff5c23c4d37ee5db2828
Markus Frohme <markus.frohme@udo.edu>
2025-12-15 18:02:45 +0100

    do not use special characters in documentation

--- util/src/main/java/net/automatalib/util/automaton/conformance/KWayTransitionCoverTestsIterator.java
+++ util/src/main/java/net/automatalib/util/automaton/conformance/KWayTransitionCoverTestsIterator.java
@@ -162,7 +162,8 @@ public class KWayTransitionCoverTestsIterator<S, I, A extends DeterministicAutom
      * @param maxPathLen
      *         the maximum step size of {@link GenerationMethod#RANDOM randomly}-generated paths
      * @param maxNumberOfSteps
-     *         threshold for the number of steps after which no more new test words will be generated (<=0 = no limit)
+     *         threshold for the number of steps after which no more new test words will be generated (a value less than
+     *         zero means no limit)
      * @param k
      *         k value used for K-Way transitions, i.e.,the number of steps between the start and the end of a
      *         transition
